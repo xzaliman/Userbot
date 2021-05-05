a1f564072c8fcfaf2b8f9faaa2c5b6ecc279ac4b
xzaliman <83049855+xzaliman@users.noreply.github.com>
2021-05-05 23:23:52 +0700

    Update updater.py

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -214,12 +214,12 @@ async def upstream(event):
         await event.edit(
             '`Sinkronisasi Paksa Ke Kode Userbot Stabil Terbaru, Harap Tunggu .....`')
     else:
-        await event.edit('`Proses Update Userbot, Loading....1%`')
-        await event.edit('`Proses Update Userbot, Loading....20%`')
-        await event.edit('`Proses Update Userbot, Loading....35%`')
-        await event.edit('`Proses Update Userbot, Loading....77%`')
+        await event.edit('`Proses Update Userbot, Loading...1%`')
+        await event.edit('`Proses Update Userbot, Loading...20%`')
+        await event.edit('`Proses Update Userbot, Loading...35%`')
+        await event.edit('`Proses Update Userbot, Loading...77%`')
         await event.edit('`Proses Update Userbot, Updating...90%`')
-        await event.edit('`Proses Update Userbot, Mohon Menunggu Lord....100%`')
+        await event.edit('`Proses Update Userbot, Mohon Menunggu...100%`')
     if conf == "now":
         await update(event, repo, ups_rem, ac_br)
         await asyncio.sleep(10)
