c512de9351685e11c499ffc57192d6113c87f767
xzaliman <83049855+xzaliman@users.noreply.github.com>
2021-05-06 00:16:51 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -20,7 +20,7 @@ async def help(event):
         if args in CMD_HELP:
             await event.edit(str(CMD_HELP[args]))
         else:
-            await event.edit("**Maaf, Saya Tidak Punya Perintah Itu ツ**")
+            await event.edit("**Saya Tidak Punya Perintah Itu**")
             await asyncio.sleep(200)
             await event.delete()
     else:
